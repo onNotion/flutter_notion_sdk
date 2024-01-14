9ca0b1bb085a81058d387e76674d9aaea7e4d3ec
alirezat66 <alirezataghizadeh66@gmail.com>
2024-01-14 18:32:45 +0100

    add new searchs

--- lib/search_api.dart
+++ lib/search_api.dart
@@ -4,10 +4,34 @@ import 'package:flutter_notion_sdk/notion_client.dart';
 class SearchApi {
   final NotionClient client = NotionClient();
 
-  Future<Map<String, dynamic>> search({required String query}) async {
+  Future<Map<String, dynamic>> searchPages({required String query}) async {
     var body = {
       "query": "",
       "filter": {"value": "page", "property": "object"},
+      "sort": {"direction": "ascending", "timestamp": "last_edited_time"}
+    };
+    try {
+      var response = await client.sendRequest(
+        endpoint: 'search',
+        method: 'POST',
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        String responseBody = await response.stream.bytesToString();
+        return json.decode(responseBody) as Map<String, dynamic>;
+      } else {
+        throw Exception(
+            'Failed to retrieve the page: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+  Future<Map<String, dynamic>> searchDatabases({required String query}) async {
+    var body = {
+      "query": "",
+      "filter": {"value": "database", "property": "object"},
       "sort": {"direction": "ascending", "timestamp": "last_edited_time"}
     };
     try {
